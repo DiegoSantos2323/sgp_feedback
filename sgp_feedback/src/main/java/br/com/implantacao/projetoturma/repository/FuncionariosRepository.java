a48fdebb055fd38d4205f3afb161567637bcef4f
Acesso Livre <Acesso Livre@BC7N278824>
2026-06-10 20:43:04 -0300

    adasd

--- sgp_feedback/src/main/java/br/com/implantacao/projetoturma/repository/FuncionariosRepository.java
+++ sgp_feedback/src/main/java/br/com/implantacao/projetoturma/repository/FuncionariosRepository.java
@@ -1,5 +1,7 @@
 package br.com.implantacao.projetoturma.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,8 @@ import br.com.implantacao.projetoturma.entity.FuncionariosEntity;
 @Repository
 public interface FuncionariosRepository extends JpaRepository<FuncionariosEntity, Long>{
 
+	List<FuncionariosEntity> findByNomeContainingIgnoreCase(String nome);
+	List<FuncionariosEntity> findByCpfContainingIgnoreCase(String cpf);
+	List<FuncionariosEntity> findByMatriculaContainingIgnoreCase(String matricula);
+	
 }
